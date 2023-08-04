da79fed85401dd36b414241648a7ca1a0f0601f6
Duncan Beyer <duncan011217@gmail.com>
2023-08-04 11:47:52 -0700

    Version 5

---
+++ v5/ProjectEmulator-main/headers/cart.h
@@ -0,0 +1,38 @@
+#pragma once
+
+#include <common.h>
+
+typedef struct {
+    u8 entry[4]; // 0x0100 - 0x0103 - Entry Point
+    u8 logo[0x30]; // 0x0103 - 0x0133 - Nintendo Logo
+
+    char title[16]; // 0x0134 - 0x0143 - Title
+    // Title of the game is in upper case ASCII
+    u16 new_lic_code; // 0x0144 - 0x0145 - New Licensee Code
+    u8 sgb_flag; // 0x0146 - Super Game Boy Functionality
+    u8 type; // 0x0147 - Cartridge Type
+    u8 rom_size; // 0x0148 - ROM Size (how much is on the cart)
+    u8 ram_size; // 0x0149 - RAM Size (how much is on the cart)
+    u8 dest_code; // 0x014A - Destination Code
+    u8 lic_code; // 0x014B - Old Licensee Code
+    // If the Old License Code is 0x33, this is the 
+    // indication that a New licensee code must be used.
+
+    u8 version; // 0x014C - Version Number (usually 0)
+    u8 checksum; // 0x014D - Checksum 
+    // Checksum must match this verification algorithm:
+    // uint8_t checksum = 0;
+    // for (uint16_t address = 0x0134; address <= 0x014C; address++) {
+    //     checksum = checksum - rom[address] - 1;
+    // } 
+
+    u16 global_checksum; // 0x014E - 0x014F - This 
+    // checksum is not used except for Pokemon Stadium's 
+    // emulator, so it is not really used here.
+} rom_header;
+
+bool cart_load(char *cart);
+
+u8 cart_read(u16 address);
+void cart_write(u16 address, u8 value);
+
---
+++ v5/ProjectEmulator-main/lib/bus.c
@@ -0,0 +1,105 @@
+#include <bus.h>
+#include <cart.h>
+#include <ram.h>
+#include <cpu.h>
+#include <io.h>
+#include <ppu.h>
+#include <dma.h>
+                                          
+u8 bus_read(u16 address) {
+    if (address < 0x8000) { 
+        // Range of ROM Bank Data
+        return cart_read(address);
+    }
+    else if (address < 0xA000) { 
+        // Character / Background Map Data
+        return ppu_vram_read(address);
+    }
+    else if (address < 0xC000) { 
+        // Cartridge Ram
+        return cart_read(address);
+    }
+    else if (address < 0xE000) { 
+        // Working Ram
+        return wram_read(address);
+    }
+    else if (address < 0xF000) { 
+        // Reserved Echo Ram
+        return 0;
+    }
+    else if (address < 0xFEA0) {
+        // Object Attribute Memory
+        if (dma_transferring()) {
+            return 0xFF;
+        }
+        return ppu_oam_read(address);
+    }
+    else if (address < 0xFF00) {
+        // reserved unusable
+        return 0;
+    }
+    else if (address < 0xFF80) {
+        return io_read(address);
+    }
+    else if (address == 0xFFFF) {
+        return cpu_get_ie_reg();
+    }
+    return hram_read(address);
+}
+
+u16 bus_read16(u16 address) {
+    u16 lo = bus_read(address);
+    u16 hi = bus_read(address + 1);
+
+    return lo | (hi << 8);
+}
+
+void bus_write(u16 address, u8 value) {
+    if (address < 0x8000) {
+        // Range of ROM Bank Data
+        cart_write(address, value);
+    }
+    else if (address < 0xA000) { 
+        // Character / Background Map Data
+        ppu_vram_write(address, value);
+    }
+    else if (address < 0xC000) { 
+        // Cartridge Ram
+        cart_write(address, value);
+    }
+    else if (address < 0xE000) { 
+        // Working Ram
+        wram_write(address, value);
+    }
+    else if (address < 0xFE00) { 
+        // Reserved Echo Ram
+        //NO_IMPL;
+    }
+    else if (address < 0xFEA0) {
+        // Object Attribute Ram
+        if (dma_transferring()) {
+            return;
+        }
+        ppu_oam_write(address, value);
+    }
+    else if (address < 0xFF00) {
+        // Unusable Reserved
+    }
+    else if (address < 0xFF80) {
+        // IO Registers
+        io_write(address, value);
+    }
+    else if (address == 0xFFFF) {
+        // Object Attribute Ram
+        cpu_set_ie_reg(value);
+    }
+    else {
+        hram_write(address,value);
+    }
+}
+
+void bus_write16(u16 address, u16 value) {
+    
+    bus_write(address + 1, (value >> 8) & 0xFF); // Write the high byte 
+    bus_write(address, 0xFF & value); // Write the low byte
+}
